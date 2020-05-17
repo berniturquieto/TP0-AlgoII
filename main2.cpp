1ca7248ac45e1907f64303c208a84ac57da1ab33
Hitalio <31748589+Hitalio@users.noreply.github.com>
2020-05-17 16:44:03 -0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+#include <cmdline.h>
+
 #include "image.h"
 
-
 using namespace std;
+
+static void opt_input(string const &);
+static void opt_output(string const &);
+static void opt_function(string const &);
+static void opt_help(string const &);
 
 
 /*static istream *iss = 0;	// Input Stream (clase para manejo de los flujos de entrada)
@@ -11,8 +21,103 @@ static fstream ifs; 		// Input File Stream (derivada de la clase ifstream que de
 static fstream ofs;		// Output File Stream (derivada de la clase ofstream que deriva de ostream para el manejo de archivos)
 */
 
-int main (){
+static option_t options[] = {
+	{1, "i", "input", "-", opt_input, OPT_DEFAULT},
+	{1, "o", "output", "-", opt_output, OPT_DEFAULT},
+	{1, "f", "function", NULL, opt_function, OPT_DEFAULT},
+	{0, "h", "help", NULL, opt_help, OPT_DEFAULT},
+	{0, },
+};
 
+static int factor;	// HAY Q CAMBIAR ESTO
+static istream *iss = 0;	// Input Stream (clase para manejo de los flujos de entrada)
+static ostream *oss = 0;	// Output Stream (clase para manejo de los flujos de salida)
+static fstream ifs; 		// Input File Stream (derivada de la clase ifstream que deriva de istream para el manejo de archivos)
+static fstream ofs;		// Output File Stream (derivada de la clase ofstream que deriva de ostream para el manejo de archivos)
+
+static void opt_input(string const &arg){
+	// Si el nombre del archivos es "-", usaremos la entrada
+	// estándar. De lo contrario, abrimos un archivo en modo
+	// de lectura.
+	//
+	if (arg == "-") {
+		iss = &cin;		// Establezco la entrada estandar cin como flujo de entrada
+	}
+	else {
+		ifs.open(arg.c_str(), ios::in); // c_str(): Returns a pointer to an array that contains a null-terminated
+										// sequence of characters (i.e., a C-string) representing
+										// the current value of the string object.
+		iss = &ifs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!iss->good()) {
+		cerr << "cannot open "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+}
+
+static void opt_output(string const &arg){
+	// Si el nombre del archivos es "-", usaremos la salida
+	// estándar. De lo contrario, abrimos un archivo en modo
+	// de escritura.
+	//
+	if (arg == "-") {
+		oss = &cout;	// Establezco la salida estandar cout como flujo de salida
+	} else {
+		ofs.open(arg.c_str(), ios::out);
+		oss = &ofs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!oss->good()) {
+		cerr << "cannot open "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);		// EXIT: Terminación del programa en su totalidad
+	}
+}
+
+static void opt_function(string const &arg){
+	istringstream iss(arg);
+
+	// Intentamos extraer la funcion de la línea de comandos.
+	// Para detectar argumentos que únicamente consistan de
+	// números enteros, vamos a verificar que EOF llegue justo
+	// después de la lectura exitosa del escalar.
+	//
+	if (!(iss >> factor)
+	    || !iss.eof()) {
+		cerr << "non-integer factor: "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+
+	if (iss.bad()) {
+		cerr << "cannot read integer factor."
+		     << endl;
+		exit(1);
+	}
+}
+
+static void opt_help(string const &arg){
+	cout << "cmdline -f function [-i file] [-o file]"
+	     << endl;
+	exit(0);
+}
+
+
+int main(int argc, char * const argv[]){
+	cmdline cmdl(options);	// Objeto con parametro tipo option_t (struct) declarado globalmente. Ver línea 51 main.cc
+	cmdl.parse(argc, argv); // Metodo de parseo de la clase cmdline
 
 	int w = 24, h = 7, gs = 10;
 	image image_in(w,h,gs);
